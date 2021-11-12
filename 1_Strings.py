9c3d60188cc5241047a95261db980d574aa83e6b
rwiteshbera <rwiteshbera@gmail.com>
2021-11-12 19:04:50 +0530

    first commit

---
+++ 1_Strings.py
@@ -0,0 +1,63 @@
+message = 'HelloWorld'
+# message1 = 'XYZ\'s House'
+
+# Print Message
+# print(message)
+
+
+
+# Print the length
+# print(len(message))
+
+
+
+# Print First Character
+# print(message[0])
+
+
+
+# Print Specific Substring between a range
+# Starting index is inclusive but ending is exclusive
+# print(message[:6]) 
+# or 
+# print(message[0:6])
+
+
+
+# Print all character in lowercase
+# print(message.lower())
+
+
+
+# Print all character in uppercase
+# print(message.upper())
+
+
+
+# Count the appearance of string or character
+# print(message.count("Hello"))
+# print(message.count('o'))
+
+
+
+
+# Find the string
+# print(message.find("World")) ## Return 5 because 'World' starts from index 5
+
+
+
+
+# Replace character or substring
+# new_msg = message.replace('o', 'X')
+# print(new_msg)
+
+
+
+# Concatenate Strings
+firstName = "Rwitesh"
+lastName = "Bera"
+Name = firstName + " " + lastName
+print(Name)
+
+
+
